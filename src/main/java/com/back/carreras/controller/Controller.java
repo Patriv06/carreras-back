f778d97dab5f1a7693f2a601a6bde5254c6559fe
Patriv06 <privas06@gmail.com>
2023-03-07 14:56:18 -0300

    upload de imagen

--- src/main/java/com/back/carreras/controller/Controller.java
+++ src/main/java/com/back/carreras/controller/Controller.java
@@ -40,7 +40,7 @@ import com.back.carreras.service.ISponsorsService;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
+
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
---
+++ src/main/java/com/back/carreras/repository/ImageRepository.java
@@ -0,0 +1,11 @@
+
+package com.back.carreras.repository;
+
+import com.back.carreras.model.ImageModel;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<ImageModel, Long> {
+	Optional<ImageModel> findByName(String name);
+    
+}
