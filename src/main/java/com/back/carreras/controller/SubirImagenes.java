f9edba2eb8ed04bedd7778af5b9fadfe96fdd2db
Patriv06 <privas06@gmail.com>
2023-03-08 15:50:18 -0300

    para bajar la imagen

--- src/main/java/com/back/carreras/controller/SubirImagenes.java
+++ src/main/java/com/back/carreras/controller/SubirImagenes.java
@@ -2,10 +2,18 @@
 package com.back.carreras.controller;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +43,26 @@ public class SubirImagenes {
 
    
 }
+@GetMapping("/imagen/{nombreArchivo}")
+public ResponseEntity<ByteArrayResource> obtenerImagen(@PathVariable String nombreArchivo) throws IOException {
+    String rutaImagen = "/src/main/resources/static/image/" + nombreArchivo; // Cambia esto por la ruta completa en tu servidor
+    File archivo = new File(rutaImagen);
+    if (!archivo.exists()) {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+    byte[] imagenBytes = Files.readAllBytes(archivo.toPath());
+    ByteArrayResource recurso = new ByteArrayResource(imagenBytes);
+    return ResponseEntity.ok()
+            .headers(headers)
+            .contentLength(imagenBytes.length)
+            .body(recurso);
+}
+}
 
-}  
+
+
+
+
+
