7554819800dfc2b72577674a43052f1c9717e15e
Patriv06 <privas06@gmail.com>
2023-03-08 09:20:58 -0300

    upload terminado

--- src/main/java/com/back/carreras/controller/SubirImagenes.java
+++ src/main/java/com/back/carreras/controller/SubirImagenes.java
@@ -1,6 +1,10 @@
 
 package com.back.carreras.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,11 +15,15 @@ import org.springframework.web.multipart.MultipartFile;
 public class SubirImagenes {
     @PostMapping("/upload-file")
             @CrossOrigin(origins={"https://rankingpilotos.web.app","http://localhost:4200","https://ranking-backoffice.web.app", "https://carreras-app-aoh3.vercel.app/"} )
-    public String uploadImage(@RequestParam("file") MultipartFile file){
+    public String uploadImage(@RequestParam("file") MultipartFile file)throws Exception{
         System.out.println(file.getOriginalFilename());
         System.out.println(file.getName());
         System.out.println(file.getContentType());
         System.out.println(file.getSize());
+
+  
+   String  Path_Directory="\\src\\main\\resources\\src\\image";
+     Files.copy(file.getInputStream(), Paths.get(Path_Directory+File.separator+file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
         return "se subió bien";
 
     }
