e2820b6e03c040ad5a922c775c1266e38ed4aa31
Patriv06 <privas06@gmail.com>
2023-03-08 12:59:50 -0300

    a ver si es esto

--- src/main/java/com/back/carreras/controller/SubirImagenes.java
+++ src/main/java/com/back/carreras/controller/SubirImagenes.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,10 +22,9 @@ public class SubirImagenes {
         System.out.println(file.getSize());
 
   
-   String  Path_Directory=new ClassPathResource("templates/").getFile().getAbsolutePath();
-  
+   String  Path_Directory="\\src\\main\\resources\\static\\image";
      Files.copy(file.getInputStream(), Paths.get(Path_Directory+File.separator+file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
-        return "se subió bien"+Path_Directory;
+        return "se subió bien";
 
     }
     
