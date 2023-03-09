2ba5b22e461df80dc69d2873cd0ee5916855af67
Patriv06 <privas06@gmail.com>
2023-03-09 13:45:43 -0300

    carga en base de datos

--- src/main/java/com/back/carreras/controller/ImageUploadController.java
+++ src/main/java/com/back/carreras/controller/ImageUploadController.java
@@ -12,6 +12,7 @@ import java.util.zip.Inflater;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -28,72 +29,33 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
      @CrossOrigin(origins={"https://rankingpilotos.web.app","http://localhost:4200","https://ranking-backoffice.web.app", "https://carreras-app-aoh3.vercel.app/"} )
 
-@RequestMapping(path = "image")
+
+@RequestMapping("/image")
 public class ImageUploadController {
 
 	@Autowired
-	ImageRepository imageRepository;
+	private ImageRepository imageRepository;
 
 	@PostMapping("/upload")
-        @CrossOrigin(origins={"https://rankingpilotos.web.app","http://localhost:4200","https://ranking-backoffice.web.app", "https://carreras-app-aoh3.vercel.app/"} )
-
-	public BodyBuilder uplaodImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
-
-		System.out.println("Original Image Byte Size - " + file.getBytes().length);
-		ImageModel img = new ImageModel(file.getOriginalFilename(), file.getContentType(),
-				compressBytes(file.getBytes()));
-		//imageRepository.save(img);
-                imageRepository.save(img);
-		return ResponseEntity.status(HttpStatus.OK);
-	}
-
-	@GetMapping(path = { "/get/{imageName}" })
-        @CrossOrigin(origins={"https://rankingpilotos.web.app","http://localhost:4200","https://ranking-backoffice.web.app", "https://carreras-app-aoh3.vercel.app/"} )
-
-	public ImageModel getImage(@PathVariable("imageName") String imageName) throws IOException {
-
-		final Optional<ImageModel> retrievedImage = imageRepository.findByName(imageName);
-		ImageModel img = new ImageModel(retrievedImage.get().getName(), retrievedImage.get().getType(),
-				decompressBytes(retrievedImage.get().getPicByte()));
-		return img;
-	}
-
-	// compress the image bytes before storing it in the database
-	public static byte[] compressBytes(byte[] data) {
-		Deflater deflater = new Deflater();
-		deflater.setInput(data);
-		deflater.finish();
-
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-		byte[] buffer = new byte[1024];
-		while (!deflater.finished()) {
-			int count = deflater.deflate(buffer);
-			outputStream.write(buffer, 0, count);
-		}
+	public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
 		try {
-			outputStream.close();
+			ImageModel imageModel = new ImageModel(file.getOriginalFilename(), file.getContentType(), file.getBytes());
+			imageRepository.save(imageModel);
+			return ResponseEntity.ok().body("Imagen subida correctamente.");
 		} catch (IOException e) {
+			e.printStackTrace();
+			return ResponseEntity.badRequest().body("Error al subir la imagen.");
 		}
-		System.out.println("Compressed Image Byte Size - " + outputStream.toByteArray().length);
-
-		return outputStream.toByteArray();
 	}
-
-	// uncompress the image bytes before returning it to the angular application
-	public static byte[] decompressBytes(byte[] data) {
-		Inflater inflater = new Inflater();
-		inflater.setInput(data);
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-		byte[] buffer = new byte[1024];
-		try {
-			while (!inflater.finished()) {
-				int count = inflater.inflate(buffer);
-				outputStream.write(buffer, 0, count);
-			}
-			outputStream.close();
-		} catch (IOException ioe) {
-		} catch (DataFormatException e) {
-		}
-		return outputStream.toByteArray();
-	}
+        @GetMapping("/{name}")
+public ResponseEntity<byte[]> getImageByName(@PathVariable String name) {
+    Optional<ImageModel> imageModelOptional = imageRepository.findByName(name);
+    if (imageModelOptional.isPresent()) {
+        ImageModel imageModel = imageModelOptional.get();
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(imageModel.getType())).body(imageModel.getPicByte());
+    } else {
+        return ResponseEntity.notFound().build();
+    }
+}
+        
 }
