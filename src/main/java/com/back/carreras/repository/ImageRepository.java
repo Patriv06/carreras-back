f8ffcae6abf76a374ecfea088f4d7131ae5eb5d5
Patriv06 <privas06@gmail.com>
2023-03-10 16:44:38 -0300

    delete de imagen en el repository

--- src/main/java/com/back/carreras/repository/ImageRepository.java
+++ src/main/java/com/back/carreras/repository/ImageRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageRepository extends JpaRepository<ImageModel, Long> {
 	Optional<ImageModel> findByName(String name);
+        
+        void deleteByName(String name);
     
 }
